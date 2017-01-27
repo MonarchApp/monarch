c1c72bb9235242db0a57a84c648dd9bf563dfe94
Brandon Geffe <brandon.geffe@rms.com>
2017-01-26 23:30:36 -0800

    really add secret file

---
+++ api/config/prod.secret.exs
@@ -0,0 +1,7 @@
+config :foo, TransSponsor.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "username_placeholder",
+  password: "password_placeholder",
+  database: "database_placeholder",
+  hostname: "dbhost_placeholder"
+  size: 20
