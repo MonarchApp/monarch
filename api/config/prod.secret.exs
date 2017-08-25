70d63b5c110079aa8f83adcb559961d36f2d35e8
Emma Bukacek <emma.bukacek@gmail.com>
2017-08-24 21:11:39 -0700

    Peace out, Elixir. You did good.

--- api/config/prod.secret.exs
+++
@@ -1,9 +0,0 @@
-use Mix.Config
-
-config :monarch, Monarch.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "username_placeholder",
-  password: "password_placeholder",
-  database: "database_placeholder",
-  hostname: "dbhost_placeholder",
-  size: 20
--- api/lib/monarch/repo.ex
+++
@@ -1,4 +0,0 @@
-defmodule Monarch.Repo do
-  @moduledoc false
-  use Ecto.Repo, otp_app: :monarch
-end
