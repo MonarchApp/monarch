899925f18102a623131676a737dafa756b2cc39b
Emma Bukacek <emma.bukacek@gmail.com>
2017-08-27 23:54:19 -0700

    Add Knex configuration

---
+++ api/knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'monarch_dev',
+      user: 'postgres'
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  }
+};
