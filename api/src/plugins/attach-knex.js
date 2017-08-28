c0c80ef9b6494e353bf7604e35ea0be89c2efa16
Emma Bukacek <emma.bukacek@gmail.com>
2017-08-28 00:24:42 -0700

    Add plugin to attach knex to server object

---
+++ api/src/plugins/attach-knex.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Knex = require('knex');
+const knexConfiguration = require('./../../knexfile.js');
+
+const attachKnex = {};
+attachKnex.register = (server, options, next) => {
+  const environment = process.env.NODE_ENV || 'development';
+  const knex = Knex(knexConfiguration[environment]);
+
+  server.decorate('server', 'knex', knex);
+  return next();
+};
+
+attachKnex.register.attributes = {
+  name: 'attachKnex',
+  version: '0.0.0'
+};
+
+module.exports = attachKnex;
