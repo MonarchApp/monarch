1b4c4a845b9735311facb82ca8b73794ab793689
Emma Bukacek <emma.bukacek@gmail.com>
2017-08-28 00:31:24 -0700

    Add error handling to server.start in app

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,12 @@ const localConfig = require('./../config/local.json');
 const server = new Hapi.Server();
 
 server.connection(localConfig);
-server.start(function() {
+server.start(err => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    return console.log(err);
+  }
+
   // eslint-disable-next-line no-console
   console.log(`\nStarting Monarch on ${localConfig.port}...\n`);
 });
