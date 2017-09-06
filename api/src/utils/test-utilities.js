9303d1a3840a00f67ae804757aeb6e5acd0752f7
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-06 03:56:59 -0400

    Update test utilities to pass mock require instead of proxyquire

--- api/src/utils/test-utilities.js
+++ api/src/utils/test-utilities.js
@@ -3,9 +3,9 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
-const proxyquire = require('proxyquire');
+const mockRequire = require('mock-require');
 chai.use(sinonChai);
 
 const {expect} = chai;
 
-module.exports = {expect, proxyquire, sinon};
+module.exports = {expect, mockRequire, sinon};
