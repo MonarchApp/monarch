d34c5e80b247135a85bd7bce65cb880cf164f3ce
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-09 05:17:19 -0400

    Add server cleanup on SIGINT event

--- api/src/app.js
+++ api/src/app.js
@@ -20,4 +20,18 @@ server.register({
   });
 });
 
+process.on('SIGINT', () => {
+  // eslint-disable-next-line no-console
+  console.log('Shutting down Monarch server...');
+
+  server.stop(err => {
+    // eslint-disable-next-line no-console
+    console.log('Monarch has stopped.');
+    if (err) {
+      return process.exit(1);
+    }
+    process.exit(0);
+  });
+});
+
 module.exports = server;
