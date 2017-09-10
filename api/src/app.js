f518133268765e9c079e637bb12204e35fb73ad4
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-10 12:21:56 -0700

    [API] Close Hapi gracefully on SIGINT (#43)
    * Gracefully close Hapi when the `SIGINT` signal is received

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,5 @@
-'use strict';
-
 const Hapi = require('hapi');
+const co = require('co');
 const localConfig = require('./../config/local.json');
 
 const server = new Hapi.Server();
@@ -19,5 +18,20 @@ server.register({
     console.log(`\nMonarch started at ${server.info.uri}\n`);
   });
 });
+
+process.on('SIGINT', co.wrap(function*() {
+  // eslint-disable-next-line no-console
+  console.log('Shutting down Monarch server...');
+  yield server.knex.destroy();
+
+  server.stop(err => {
+    // eslint-disable-next-line no-console
+    console.log('Monarch has stopped.');
+    if (err) {
+      return process.exit(1);
+    }
+    process.exit(0);
+  });
+}));
 
 module.exports = server;
