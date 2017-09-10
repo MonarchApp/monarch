4961a96fdcce223fe4a9b93db42e93c18e564619
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-10 13:22:33 -0700

    Make migrations work in cucumber hooks

--- api/src/features/step_definitions/hooks.js
+++ api/src/features/step_definitions/hooks.js
@@ -1,26 +1,21 @@
-const co = require('co');
 const rootRequire = require('app-root-path').require;
 const {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Before, After}) {
-  Before(co.wrap(function*() {
-    process.env.NODE_ENV = 'test';
-
+  Before(function*() {
     this.server = rootRequire('src/app');
     this.knex = this.server.knex;
     yield this.knex.migrate.latest();
-  }));
+  });
 
   Before(function() {
     this.apiUrl = `${this.server.info.uri}/v1`;
   });
 
-  After(function() {
-    const knex = this.server.knex;
+  After(function*() {
+    yield this.knex.raw('DROP SCHEMA public CASCADE');
+    yield this.knex.raw('CREATE SCHEMA public');
 
-    this.server.stop(co.wrap(function*() {
-      yield knex.raw('DROP SCHEMA public CASCADE');
-      yield knex.raw('CREATE SCHEMA public');
-    }));
+    this.server.stop();
   });
 });
