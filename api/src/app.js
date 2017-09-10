b73628397fcd7f2600856e9c0e0bafa5bc10c2a6
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-10 07:04:18 -0400

    Add logging with Good

--- api/src/app.js
+++ api/src/app.js
@@ -4,14 +4,34 @@ const Hapi = require('hapi');
 const localConfig = rootRequire('config/local');
 const routes = require('./routes');
 
+const goodOptions = {
+  ops: {
+    interval: 1000
+  },
+  reporters: {
+    console: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{
+        error: '*',
+        log: '*',
+        response: '*'
+      }]
+    }, {
+      module: 'good-console'
+    }, 'stdout']
+  }
+};
+
 const server = new Hapi.Server();
 
 server.connection(localConfig);
 server.route(routes);
 
-server.register({
-  register: require('./plugins/attach-knex')
-}, (err) => {
+server.register([
+  {register: require('./plugins/attach-knex')},
+  {register: require('good'), options: goodOptions}
+], (err) => {
   if (err) {
     // eslint-disable-next-line no-console
     return console.log(err);
