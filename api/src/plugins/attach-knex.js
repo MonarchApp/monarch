a05681888aedd609b11ef9a3d27c3dc09336da53
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-15 10:02:46 -0700

    Revert "Capitalize AttachKnex plugin"
    This reverts commit 9a3cdf2c91bac11f214c366f2979c7e65e1de54f.

--- api/src/plugins/attach-knex.js
+++ api/src/plugins/attach-knex.js
@@ -2,9 +2,9 @@ const knex = require('knex');
 const rootRequire = require('app-root-path').require;
 const knexConfiguration = rootRequire('knexfile');
 
-const AttachKnex = {};
+const attachKnex = {};
 
-AttachKnex.register = (server, options, next) => {
+attachKnex.register = (server, options, next) => {
   const environment = process.env.NODE_ENV || 'development';
   const knexConn = knex(knexConfiguration[environment]);
 
@@ -13,9 +13,9 @@ AttachKnex.register = (server, options, next) => {
   next();
 };
 
-AttachKnex.register.attributes = {
-  name: 'AttachKnex',
+attachKnex.register.attributes = {
+  name: 'attachKnex',
   version: '0.0.0'
 };
 
-module.exports = AttachKnex;
+module.exports = attachKnex;
