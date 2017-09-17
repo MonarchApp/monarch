ec394666b50dd779cfd0fa208c82be1f61935c45
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-17 08:42:22 -0700

    Update server to use async

--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,10 @@
 const rootRequire = require('app-root-path').require;
 const Hapi = require('hapi');
-const co = require('co');
 
 const localConfig = rootRequire('config/local');
 const routes = require('./routes');
 
-const initServer = co.wrap(function(options) {
+const initServer = async (options) => {
   const server = new Hapi.Server();
   const optionsWithDefaults = Object.assign({}, localConfig, options);
   const {connection: serverOptions, good: goodOptions} = optionsWithDefaults;
@@ -26,22 +25,21 @@ const initServer = co.wrap(function(options) {
     });
   });
 
-  process.on('SIGINT', co.wrap(function*() {
+  process.on('SIGINT', async () => {
     // eslint-disable-next-line no-console
     console.log('Shutting down Monarch server...');
-    yield server.knex.destroy();
+    await server.knex.destroy();
 
     server.stop(err => {
       // eslint-disable-next-line no-console
       console.log('Monarch has stopped.');
-      if (err) {
-        return process.exit(1);
-      }
+
+      if (err) { return process.exit(1); }
       process.exit(0);
     });
-  }));
+  });
 
   return server;
-});
+};
 
 module.exports = initServer;
