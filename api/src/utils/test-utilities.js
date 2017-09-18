646a97478bc8e2be24957e4b8ea16d8287d71ad4
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-18 09:31:22 -0700

    Add chaiAsPromised to test utilities

--- api/src/utils/test-utilities.js
+++ api/src/utils/test-utilities.js
@@ -2,8 +2,10 @@ const rootRequire = require('app-root-path').require;
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
+const chaiAsPromised = require('chai-as-promised');
 const mockRequire = require('mock-require');
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 
 const {expect} = chai;
 
