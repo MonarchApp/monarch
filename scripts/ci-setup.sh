835eba5ed4cd9def05181ad70d4fb34f6ab3beff
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-17 21:54:59 -0700

    Maybe?

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+
 # Node
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
-echo 'what it be'
-node -v
 
 # Database
 createdb 'monarch_test'
 
 # Install
-sudo apt-get update
 npm install -g yarn
