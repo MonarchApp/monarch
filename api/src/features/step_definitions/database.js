b926e9196845e66c274225933bf60ae846c84d34
Emma Bukacek <emma.bukacek@gmail.com>
2017-09-24 03:52:30 -0700

    Add step for seeding data

--- api/src/features/step_definitions/database.js
+++ api/src/features/step_definitions/database.js
@@ -1,4 +1,5 @@
 const matchPattern = require('lodash-match-pattern');
+const rootRequire = require('app-root-path').require;
 const {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Then, When}) {
@@ -8,6 +9,11 @@ defineSupportCode(function({Then, When}) {
 
   When('{string} table is dropped', async function(tableName) {
     await this.knex.schema.dropTable(tableName);
+  });
+
+  When('I seed {string}', async function(seedFilename) {
+    const pathToSeed = `src/db/seeds/${process.env.NODE_ENV}/${seedFilename}`;
+    await rootRequire(pathToSeed).seed(this.knex);
   });
 
   Then('raw query result matches', async function(json) {
