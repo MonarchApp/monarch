059a6c7ce43de180bacc37857f9c665d4b1a86d6
Emma Bukacek <emma.bukacek@gmail.com>
2017-10-13 01:14:45 -0700

    Add basic example splash page

--- app/src/containers/root/index.js
+++ app/src/containers/root/index.js
@@ -1,8 +1,15 @@
+import './root.scss';
 import React from 'react';
 
 const Root = () => (
-  <div>
-    <h1>Monarch</h1>
+  <div className='wrap'>
+    <div className='banner'>
+      <h1>Monarch</h1>
+      <p>
+        A community for transgender people.
+        A platform to talk or an avenue to listen.
+      </p>
+    </div>
   </div>
 );
 Root.displayName = 'Root';
