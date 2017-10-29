664855eed6c04528543f2cf2d221c42344ec70fd
Emma Bukacek <emma.bukacek@gmail.com>
2017-10-29 02:03:54 -0700

    Add basic mocha options

---
+++ app/test/init.js
@@ -0,0 +1,9 @@
+import Chai from 'chai';
+import ChaiAsPromised from 'chai-as-promised';
+import SinonChai from 'sinon-chai';
+
+Chai.use(SinonChai);
+Chai.use(ChaiAsPromised);
+
+global.expect = Chai.expect;
+global.window = {};
