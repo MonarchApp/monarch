d2ef7c1e69c62620beb2410daabfbf8b8daa2155
Emma Bukacek <emma.bukacek@gmail.com>
2017-12-17 12:42:24 -0800

    Add mock-require, redux-mock-store, redux-promise-middleware, and redux-thunk

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import asyncAwait from 'redux-async-await';
-import {combineReducers} from 'redux-immutablejs';
 import apiMiddleware from 'middleware/api';
+import promiseMiddleware from 'redux-promise-middleware';
 import reducers from 'reducers';
+import thunk from 'redux-thunk';
+import {combineReducers} from 'redux-immutablejs';
+import {createStore, applyMiddleware} from 'redux';
 
 import 'theme/main.scss';
 import Root from 'containers/root';
 
 const store = createStore(
   combineReducers(reducers),
-  applyMiddleware(asyncAwait)(apiMiddleware)
+  applyMiddleware(thunk)(promiseMiddleware)(apiMiddleware)
 );
 
 ReactDOM.render(
