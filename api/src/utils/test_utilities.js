9c09353339f52b232105dd9f5ee3366a3b60dff4
Emma Bukacek <emma.bukacek@gmail.com>
2017-12-26 23:01:16 -0800

    Undo all the test utilities stuff

--- api/src/utils/test_utilities.js
+++ api/src/utils/test_utilities.js
@@ -1,37 +1,4 @@
-const _ = require('lodash');
 const rootRequire = require('app-root-path').require;
+const getKnexConnection = () => require('knex')(rootRequire('knexfile')['test']);
 
-const getJoiStubFunctionArgs = args => {
-  if (args === []) return '()';
-  return `(${args.join(', ')})`;
-};
-
-const testUtils = {};
-
-testUtils.getKnexConnection = () => require('knex')(rootRequire('knexfile')['test']);
-
-testUtils.getJoiStub = methods => {
-  return methods.reduce((stub, methodName) => {
-    stub[methodName] = function(...args) {
-      const functionArgs = getJoiStubFunctionArgs(args);
-      this.validationString += methodName + functionArgs;
-      return this;
-    };
-    return stub;
-  }, {});
-};
-
-testUtils.validateJoiStub = (joiStub, methods) => {
-  methods.forEach((method) => {
-    let methodName = method;
-    let args = [];
-
-    if (_.isArray(method)) [methodName, ...args] = method;
-
-    const functionArgs = getJoiStubFunctionArgs(args);
-    expect(joiStub.validationString).to.contain(methodName + functionArgs);
-  });
-};
-
-
-module.exports = testUtils;
+module.exports = {getKnexConnection};
