9253a963e5941cb427913c5f5b592533ec37313c
Emma Bukacek <emma.bukacek@gmail.com>
2017-12-28 23:23:24 -0800

    Update request to take a token and add step to parse response as json

--- api/src/features/step_definitions/requests.js
+++ api/src/features/step_definitions/requests.js
@@ -6,14 +6,18 @@ defineSupportCode(function({When, Then}) {
   When('{getOrDelete} {string}', async function(method, requestPath) {
     const url = this.utils.getRequestUrl(requestPath);
 
-    this.activeRequest = await this.utils.request({method, url});
+    this.activeRequest = await this.utils.request({method, url}, this.token);
   });
 
   When('{postOrPut} {string}', async function(method, requestPath, json) {
     const url = this.utils.getRequestUrl(requestPath);
     const body = this.utils.parseJson(json);
 
-    this.activeRequest = await this.utils.request({body, json: true, method, url});
+    this.activeRequest = await this.utils.request({body, json: true, method, url}, this.token);
+  });
+
+  When('response body is parsed as json', function() {
+    this.activeRequest.body = this.utils.parseJson(this.activeRequest.body);
   });
 
   Then('response status code is {int}', function(statusCode) {
