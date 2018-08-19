ca4dab57d9fe609919c4a5e01c7fa47839c574d3
Emma Bukacek <emma.bukacek@gmail.com>
2018-08-18 17:11:51 -0700

    [Scripts] Remove all
    * Remove all `ci-*.sh` scripts

--- scripts/ci-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd api || exit
-yarn
-yarn gen-keys
-cd ../ || exit
--- scripts/ci-test-report.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd api || exit
-yarn run coverage | ./node_modules/.bin/coveralls
-cd ../ || exit
