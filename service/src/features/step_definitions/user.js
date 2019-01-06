0787142fc4a48486ca6c08465fb3a2779a84d777
Emma Bukacek <emma.bukacek@gmail.com>
2019-01-06 09:32:07 -0800

    Update users/patch.feature to remove instances of 'I'

--- service/src/features/step_definitions/user.js
+++ service/src/features/step_definitions/user.js
@@ -9,12 +9,12 @@ Given('{ordinalInt} user matches', async function(id, json) {
   if (userResultMatchesPattern) { throw new Error(userResultMatchesPattern); }
 });
 
-Given('I store the {ordinalInt} user', async function(id) {
+Given('a comparison of the {ordinalInt} user', async function(id) {
   const [user] = await this.knex('user_account').select().where({id});
   this.activeUser = user;
 });
 
-Then('{ordinalInt} user remains unchanged', async function(id) {
+Then('{ordinalInt} user has not changed', async function(id) {
   const [user] = await this.knex('user_account').select().where({id});
   expect(user).to.eql(this.activeUser);
 });
