0dfea56c8b116fb8de0d186c343f5cbf6a16c1d5
Emma Bukacek <emma.bukacek@gmail.com>
2019-01-07 13:07:28 -0800

    Update seeds to use user_account_info

--- service/env/development/seeds/user_account.js
+++ service/env/development/seeds/user_account.js
@@ -6,11 +6,16 @@ const {saltRounds} = require('../config.json').auth;
 const hash = Promise.promisify(bcrypt.hash);
 
 exports.seed = async knex => {
-  const users = [{
-    email: 'jill@abc.com',
-    id: uuidv4(),
-    password: await hash('password', saltRounds)
-  }];
+  const userId = uuidv4();
 
-  await knex('user_account').insert(users);
+  await knex('user_account').insert({
+    id: userId,
+    password: await hash('password', saltRounds)
+  });
+
+  await knex('user_account_info').insert({
+    id: uuidv4(),
+    user_account_id: userId,
+    email: 'jill@abc.com',
+  });
 };
--- service/env/test/seeds/user_account.js
+++ service/env/test/seeds/user_account.js
@@ -1,17 +1,20 @@
 const Promise = require('bluebird');
 const bcrypt = require('bcrypt');
+const uuidv4 = require('uuid/v4');
 
 const {saltRounds} = require('../config.json').auth;
 const hash = Promise.promisify(bcrypt.hash);
 
 exports.seed = async knex => {
-  const users = [
-    {
-      email: 'frankjaeger@foxhound.com',
-      id: '10ba038e-48da-487b-96e8-8d3b99b6d18a',
-      password: await hash('password', saltRounds)
-    }
-  ];
+  const userId = '10ba038e-48da-487b-96e8-8d3b99b6d18a';
+  await knex('user_account').insert({
+    id: userId,
+    password: await hash('password', saltRounds)
+  });
 
-  return knex('user_account').insert(users);
+  await knex('user_account_info').insert({
+    id: uuidv4(),
+    user_account_id: userId,
+    email: 'frankjaeger@foxhound.com',
+  });
 };
