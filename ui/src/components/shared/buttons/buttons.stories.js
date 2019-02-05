1c4179e4bfb63f24dbcf634b560da91d35830f02
Emma Bukacek <emma.bukacek@gmail.com>
2019-02-04 19:22:13 -0800

    Remove unnecessary React.Fragment from Button stories

--- ui/src/components/shared/buttons/buttons.stories.js
+++ ui/src/components/shared/buttons/buttons.stories.js
@@ -4,17 +4,15 @@ import {PrimaryButton} from './index.js';
 
 storiesOf('Components/Buttons', module)
   .add('Primary Button', () =>
-    <React.Fragment>
-      <dl className='col-4'>
-        <dt>Enabled</dt>
-        <dd>
-          <PrimaryButton text='Button' />
-        </dd>
-        <dt>Disabled</dt>
-        <dd>
-          <PrimaryButton text='Button' disabled />
-        </dd>
-      </dl>
-    </React.Fragment>
+    <dl className='col-4'>
+      <dt>Enabled</dt>
+      <dd>
+        <PrimaryButton text='Button' />
+      </dd>
+      <dt>Disabled</dt>
+      <dd>
+        <PrimaryButton text='Button' disabled />
+      </dd>
+    </dl>
   );
 
