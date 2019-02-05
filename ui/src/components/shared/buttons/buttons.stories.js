28d9bd484aef1dd3ed73438348316f6cb263b87a
Emma Bukacek <emma.bukacek@gmail.com>
2019-02-05 07:52:34 -0800

    Flatten button stories

--- ui/src/components/shared/buttons/buttons.stories.js
+++ ui/src/components/shared/buttons/buttons.stories.js
@@ -2,17 +2,20 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {PrimaryButton} from './index.js';
 
-storiesOf('Components/Buttons', module)
-  .add('Primary Button', () =>
-    <dl className='col-4'>
-      <dt>Enabled</dt>
-      <dd>
-        <PrimaryButton text='Button' />
-      </dd>
-      <dt>Disabled</dt>
-      <dd>
-        <PrimaryButton text='Button' disabled />
-      </dd>
-    </dl>
+storiesOf('Components/Shared', module)
+  .add('Button', () =>
+    <div className='col-4'>
+      <h3>Primary</h3>
+      <dl>
+        <dt>Enabled</dt>
+        <dd>
+          <PrimaryButton text='Button' />
+        </dd>
+        <dt>Disabled</dt>
+        <dd>
+          <PrimaryButton text='Button' disabled />
+        </dd>
+      </dl>
+    </div>
   );
 
