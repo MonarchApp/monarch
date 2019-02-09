ac3d4179b3ce9a55653af880ef159f44dc357a98
Emma Bukacek <emma.bukacek@gmail.com>
2019-02-08 18:49:07 -0800

    Use transactions for seeds

--- service/env/development/seeds/user_account.js
+++ service/env/development/seeds/user_account.js
@@ -7,15 +7,16 @@ const hash = Promise.promisify(bcrypt.hash);
 
 exports.seed = async knex => {
   const userId = uuidv4();
+  const password = await hash('password', saltRounds);
 
-  await knex('user_account').insert({
-    id: userId,
-    password: await hash('password', saltRounds)
-  });
-
-  await knex('user_account_info').insert({
-    id: uuidv4(),
-    user_account_id: userId,
-    email: 'jill@abc.com',
-  });
+  await knex.transaction(trx =>
+    Promise.all([
+      trx('user_account').insert({id: userId, password}),
+      trx('user_account_info').insert({
+        id: uuidv4(),
+        user_account_id: userId,
+        email: 'jill@abc.com',
+      })
+    ])
+  );
 };
--- service/env/test/seeds/user_account.js
+++ service/env/test/seeds/user_account.js
@@ -7,14 +7,16 @@ const hash = Promise.promisify(bcrypt.hash);
 
 exports.seed = async knex => {
   const userId = '10ba038e-48da-487b-96e8-8d3b99b6d18a';
-  await knex('user_account').insert({
-    id: userId,
-    password: await hash('password', saltRounds)
-  });
+  const password = await hash('password', saltRounds);
 
-  await knex('user_account_info').insert({
-    id: uuidv4(),
-    user_account_id: userId,
-    email: 'frankjaeger@foxhound.com',
-  });
+  await knex.transaction(trx =>
+    Promise.all([
+      trx('user_account').insert({id: userId, password}),
+      trx('user_account_info').insert({
+        id: uuidv4(),
+        user_account_id: userId,
+        email: 'frankjaeger@foxhound.com',
+      })
+    ])
+  );
 };
