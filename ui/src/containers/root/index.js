76802890792bc02a932fbcbc999d825fa5074326
Emma Bukacek <emma.bukacek@gmail.com>
2019-02-14 11:25:19 -0800

    Set up initial routes and hoist store into root component

--- ui/src/containers/root/index.js
+++ ui/src/containers/root/index.js
@@ -1,7 +1,31 @@
-import Login from 'containers/identity/login';
-import React from 'react';
 import './root.scss';
+import Login from 'containers/identity/login';
+import PropTypes from 'prop-types';
+import React from 'react';
+import RoutePaths from 'constants/route_paths.js';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
-const Root = () => <Login />;
+const Routes = () =>
+  <Switch>
+    <Route
+      path={RoutePaths.IDENTITY.LOGIN}
+      component={Login} />
+    <Route
+      path={RoutePaths.HOME}
+      render={() => <h1>You did it!</h1>} />
+    <Route render={() => <Redirect to={RoutePaths.IDENTITY.LOGIN} />} />
+  </Switch>;
+
+const Root = ({store}) =>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>;
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired
+};
 
 export default Root;
