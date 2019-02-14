43174377d7c88fdee9e5f361d75b47b87e0e8dc7
Emma Bukacek <emma.bukacek@gmail.com>
2019-02-14 11:26:13 -0800

    Set up redirects after logging in successfully

--- ui/src/containers/identity/login/index.js
+++ ui/src/containers/identity/login/index.js
@@ -1,32 +1,41 @@
+import './login.scss';
+import * as recompose from 'recompose';
 import IdentityActions from './actions';
 import Login from 'components/identity/login';
+import React from 'react';
+import RoutePaths from 'constants/route_paths.js';
 import validate from './validate';
+import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {reduxForm, stopSubmit, SubmissionError} from 'redux-form';
-import './login.scss';
 
 const FORM_NAME = 'login';
 
-const LoginForm = reduxForm({
-  form: FORM_NAME,
-  initialValues: {
-    email: '',
-    password: ''
-  },
-  persistentSubmitErrors: true,
-})(Login);
+export default recompose.compose(
+  connect(
+    state => ({authenticated: state.identity.authenticated}),
+    dispatch => ({
+      onSubmit: form => {
+        const validated = validate(form);
+        if (Object.keys(validated).length) {
+          return Promise.reject(new SubmissionError(validated));
+        }
 
-export default connect(
-  null,
-  dispatch => ({
-    onSubmit: form => {
-      const validated = validate(form);
-      if (Object.keys(validated).length) {
-        return Promise.reject(new SubmissionError(validated));
+        dispatch(stopSubmit(FORM_NAME, {}));
+        return dispatch(IdentityActions.login(form.email, form.password));
       }
-
-      dispatch(stopSubmit(FORM_NAME, {}));
-      return dispatch(IdentityActions.login(form.email, form.password));
-    }
+    })
+  ),
+  recompose.branch(
+    props => props.authenticated,
+    recompose.renderComponent(() => <Redirect to={RoutePaths.HOME} />)
+  ),
+  reduxForm({
+    form: FORM_NAME,
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    persistentSubmitErrors: true,
   })
-)(LoginForm);
+)(Login);
