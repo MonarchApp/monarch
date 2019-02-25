b47d8d4c13583072d222e96fd9ddddd439a13f01
Emma Bukacek <emma.bukacek@gmail.com>
2019-02-24 17:57:17 -0800

    Use mdillon/postgis instead of postgres

--- service/images/postgres/setup.sh
+++ service/images/postgres/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 set -e
 
-POSTGRES="psql --username ${POSTGRES_USER}"
+POSTGRES="psql --username=${POSTGRES_USER}"
 
 for db in $POSTGRES_DBS; do
   $POSTGRES <<EOF
-CREATE DATABASE ${db} OWNER ${POSTGRES_USER};
+CREATE DATABASE ${db} OWNER ${POSTGRES_USER} TEMPLATE template_postgis;
 EOF
 done
