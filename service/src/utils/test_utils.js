ee70a6dd70ecb5a0f87286c9ed69cb13b4c05979
Emma Bukacek <emma.bukacek@gmail.com>
2019-02-24 17:58:54 -0800

    Remove database schema initialization in killKnex test util

--- service/src/utils/test_utils.js
+++ service/src/utils/test_utils.js
@@ -5,11 +5,6 @@ const testConfig = rootRequire('knexfile')['test'];
 const knexConn = knex(testConfig);
 
 const getKnexConnection = () => knexConn;
-
-const killKnex = async () => {
-  await knexConn.raw('DROP SCHEMA public CASCADE');
-  await knexConn.raw('CREATE SCHEMA public');
-  await knexConn.destroy();
-};
+const killKnex = async () => await knexConn.destroy();
 
 module.exports = {getKnexConnection, killKnex};
